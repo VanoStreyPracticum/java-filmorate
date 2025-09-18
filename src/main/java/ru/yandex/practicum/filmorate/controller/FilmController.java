f7c9a67a54409e1e85523eb545fc63daf68e4e9e
VanoStreyPracticum <vanostrey@yandex.ru>
2025-09-18 07:49:27 +0300

    .

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,36 +1,46 @@
 package ru.yandex.practicum.filmorate.controller;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.ValidationService;
+import ru.yandex.practicum.filmorate.storage.InMemoryStorage;
 
-import java.util.List;
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/films")
 @RequiredArgsConstructor
+@Slf4j
 public class FilmController {
-
-    private final FilmService filmService;
+    private final InMemoryStorage storage;
     private final ValidationService validationService;
 
     @PostMapping
-    public Film create(@Valid @RequestBody Film film) {
+    public ResponseEntity<Film> addFilm(@Valid @RequestBody Film film) {
         validationService.validateNewFilm(film);
-        return filmService.create(film);
+        Film saved = storage.addFilm(film);
+        log.info("Добавлен фильм: id={}, name='{}'", saved.getId(), saved.getName());
+        return ResponseEntity.ok(saved);
     }
 
     @PutMapping
-    public Film update(@Valid @RequestBody Film film) {
+    public ResponseEntity<Film> updateFilm(@Valid @RequestBody Film film) {
+        if (film.getId() == null || !storage.existsFilm(film.getId())) {
+            String msg = "Фильм с указанным id не найден";
+            log.warn(msg + " id=" + film.getId());
+            return ResponseEntity.notFound().build();
+        }
         validationService.validateNewFilm(film);
-        return filmService.update(film);
+        Film updated = storage.addFilm(film);
+        log.info("Обновлён фильм: id={}, name='{}'", updated.getId(), updated.getName());
+        return ResponseEntity.ok(updated);
     }
 
     @GetMapping
-    public List<Film> findAll() {
-        return filmService.findAll();
+    public Collection<Film> getAll() {
+        return storage.getAllFilms();
     }
 }
