b28d6dfad6a03040f526b7ccbaf28b7d7decfdb4
VanoStreyPracticum <vanostrey@yandex.ru>
2025-09-18 05:53:09 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,35 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.ValidationService;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
+
+    private final FilmService filmService;
+    private final ValidationService validationService;
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+        validationService.validateNewFilm(film);
+        return filmService.create(film);
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film film) {
+        validationService.validateNewFilm(film);
+        return filmService.update(film);
+    }
+
+    @GetMapping
+    public List<Film> findAll() {
+        return filmService.findAll();
+    }
 }
