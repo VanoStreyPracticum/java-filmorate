c17328d710ea923c5c643b18ae25136a174000ea
VanoStreyPracticum <vanostrey@yandex.com>
2025-11-04 00:43:04 +0300

    _

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -4,10 +4,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.dto.film.UpdateFilmRequest;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
-import ru.yandex.practicum.filmorate.mapper.FilmMapper;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
@@ -36,7 +34,8 @@ public class FilmService {
         if (film.getId() == null)
         {
             throw new ValidationException("ID обязателен для обновления фильма");
-        } validationService.validateNewFilm(film);
+        }
+        validationService.validateNewFilm(film);
         return filmStorage.updateFilm(film);
     }
 
